744a2f392c3a1aeadedf0dffa41afb1b2fd4651a
Martin Lehmann <martin@folio.no>
2021-04-08 12:45:58 +0200

    request works

--- test.js
+++ test.js
@@ -1,14 +1,48 @@
-const a = jest.fn(() => Promise.resolve());
-const b = jest.fn(() => Promise.resolve());
+// const nock = require('nock');
+const http = require('http');
+const {promisify} = require('util');
 
-async function subject() {
-    await a();
-    await b();
+const port = 6767;
+
+let server;
+
+beforeAll(done => {
+    server = http.createServer((req, res) => {
+        res.write('hello world');
+        res.end();
+    });
+    server.listen(port, done);
+});
+
+afterAll(done => {
+    server.close(done);
+});
+
+async function request() {
+    return new Promise((resolve, reject) => {
+        const req = http.request('http://localhost:6767', {}, res => {
+            const responseChunks = [];
+
+            res.on('data', chunk => {
+                responseChunks.push(chunk)
+            });
+
+            res.on('error', err => {
+                reject(err);
+            });
+
+            res.on('end', () => {
+                const response = Buffer.concat(responseChunks).toString();
+                resolve(response);
+            });
+        });
+
+        // send request
+        req.end();
+    });
 }
 
 it('works', async () => {
-   jest.useFakeTimers('modern');
-   await subject();
-   expect(a).toHaveBeenCalledTimes(1);
-   expect(b).toHaveBeenCalledTimes(1);
+    const res = await request();
+    expect(res).toBe('hello world');
 });
